33524ff2688abaa7d3ae0ef3c91486ccd822aea1
T1ish <hisuiismine@yahoo.com>
2017-11-18 22:10:48 +0100

    Insert XMLHttpRequest for HYF repos on github

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ searchButton.addEventListener('click', function(){
 	console.log("You clicked me!");
 });
 
+
+const url = 'https://api.github.com/orgs/HackYourFuture/repos';
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('load', function(data){
+	if(this.status === 200){
+		console.log(request);
+		const rawResponse = request.responseText;
+		const parsedReponse = JSON.parse(rawResponse);
+		console.log(parsedReponse);
+
+	}else {
+		console.log("Something is wrong cuz I'm not getting data!");
+	}
+
+
+
+});
+
+request.addEventListener('error', function(){
+	console.log("Some error happened!");
+});
+
+request.open('GET', url);
+
+request.send();
+
