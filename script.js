05798990605d1002057405d399228c64373f9200
T1ish <hisuiismine@yahoo.com>
2017-11-18 22:45:09 +0100

    Connect the button with XMLHttpRequest function

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
 const searchButton = document.querySelector("#search");
+const url = 'https://api.github.com/orgs/HackYourFuture/repos';
+const request = new XMLHttpRequest();
 
 searchButton.addEventListener('click', function(){
 	console.log("You clicked me!");
+	request.open('GET', url);
+	request.send();
 });
 
-
-const url = 'https://api.github.com/orgs/HackYourFuture/repos';
-
-const request = new XMLHttpRequest();
-
-request.addEventListener('load', function(data){
+request.addEventListener('load', function (data){
 	if(this.status === 200){
-		console.log(request);
+		//console.log(request);
 		const rawResponse = request.responseText;
 		const parsedReponse = JSON.parse(rawResponse);
-		console.log(parsedReponse);
 
 		shownOnSite(parsedReponse);
-
-
 	}else {
 		console.log("Something is wrong cuz I'm not getting data!");
 	}
-
-
-
 });
 
 request.addEventListener('error', function(){
 	console.log("Some error happened!");
 });
 
-request.open('GET', url);
-
-request.send();
-
-
 function shownOnSite(responseObject){
 	const resultDiv = document.querySelector('#showResult');
 
 	for(let i = 0; i < responseObject.length; i++){
 		const repoLiElement = document.createElement('li');
-
 		repoLiElement.innerHTML = responseObject[i].name;
-
 		resultDiv.appendChild(repoLiElement);
-
 	}
-
 }
