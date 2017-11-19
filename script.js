4f974fbb2b818cec70d1cae6f29dfd3077d31618
T1ish <hisuiismine@yahoo.com>
2017-11-19 09:03:30 +0100

    add search field for user on github

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
+
 const searchButton = document.querySelector("#search");
-const url = 'https://api.github.com/orgs/HackYourFuture/repos';
-const request = new XMLHttpRequest();
+
 
 searchButton.addEventListener('click', function(){
 	console.log("You clicked me!");
+
+	const queryField = document.querySelector("#query");
+	//const url = 'https://api.github.com/orgs/HackYourFuture/repos';	
+	//You need to know that it's the value attribute's result that you want to go further with. 
+	//console.log(queryField.value);
+	
+	requestUserInfo(queryField.value);
+});
+
+
+
+function requestUserInfo(userName){
+	const url = 'https://api.github.com/users/' + userName + '/repos';	
+
+	const request = new XMLHttpRequest();
+
+	request.addEventListener('load', function (data){
+		if(this.status === 200){
+			//console.log(request);
+			const rawResponse = request.responseText;
+			const parsedReponse = JSON.parse(rawResponse);
+
+			shownOnSite(parsedReponse, userName);
+		}else {
+			console.log("Something is wrong cuz I'm not getting data!");
+		}
+	});
+
+	request.addEventListener('error', function(){
+		console.log("Some error happened!");
+	});
+
 	request.open('GET', url);
 	request.send();
-});
 
-request.addEventListener('load', function (data){
-	if(this.status === 200){
-		//console.log(request);
-		const rawResponse = request.responseText;
-		const parsedReponse = JSON.parse(rawResponse);
+}
 
-		shownOnSite(parsedReponse);
-	}else {
-		console.log("Something is wrong cuz I'm not getting data!");
-	}
-});
 
-request.addEventListener('error', function(){
-	console.log("Some error happened!");
-});
 
-function shownOnSite(responseObject){
+function shownOnSite(responseObject, queryName){
 	const resultDiv = document.querySelector('#showResult');
 
 	for(let i = 0; i < responseObject.length; i++){
 		const repoLiElement = document.createElement('li');
 
         //For the repository JSON page I would've just used "url" from the object but I guess this is an exercise to concatenate string with variables to get the proper link. :) 
-		repoLiElement.innerHTML = '<a target="_blank" href="' + responseObject[i].html_url + '">' + responseObject[i].name + '</a>' + ' | ' + '<a target="_blank" href="' + 'https://api.github.com/repos/HackYourFuture/' + responseObject[i].name + '">' + "JSON page" + '</a>';
+		repoLiElement.innerHTML = '<a target="_blank" href="' + responseObject[i].html_url + '">' + responseObject[i].name + '</a>' + ' | ' + '<a target="_blank" href="' + 'https://api.github.com/repos/' + queryName + '/' + responseObject[i].name + '">' + "JSON page" + '</a>';
 		resultDiv.appendChild(repoLiElement);     
 	} 
 }
